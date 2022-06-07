73dc3a0bb9afc7d9fd7b9df51dafb413fad8312c
Cameron J <@>
2022-06-07 00:55:50 -0600

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <memory>
+#include <format>
+
+int main()
+{
+    int bytesToAllocate = 0, bytesAllocated = 0;
+    std::cout << "How many bytes would you like to allocate?" << std::endl << ">>";
+    std::cin >> bytesToAllocate;
+
+    char *heapAllocatedBytes = new char[bytesToAllocate];
+    bytesAllocated = bytesToAllocate;
+    memset(heapAllocatedBytes, 0, bytesAllocated);
+    auto currentByte = heapAllocatedBytes;
+    auto bytesEnd = heapAllocatedBytes + bytesAllocated;
+    for (; currentByte != bytesEnd; currentByte++)
+    {
+        std::cout << std::format("Address {:#x}: \"{:#x}\"\r\n", (int) currentByte, (int) *currentByte);
+    }
+    return 0;
+}
